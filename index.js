bf41173da7435bb73b5c47a0888ec077cb0884fe
Kyle Murray <kylemurray@Kyles-MacBook-Air.local>
2024-01-06 05:15:21 -0800

    changed greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var counter = 0;
 
 app.get('/', (req, res) => {
     counter++;
-    res.send({ yo: "this page has been visited " + counter + " times" });
+    res.send({ yo: "this page has not been visited " + counter + " times" });
 });
 
 const PORT = process.env.PORT || 5000;
